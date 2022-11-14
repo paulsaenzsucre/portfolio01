b0aca975c7efee32e02112f403dd1d2912b1c7ca
emmanuelcortes2 <109642972+emmanuelcortes2@users.noreply.github.com>
2022-11-14 14:03:56 -0600

    add 'ocultarMenu' function

--- js/script.js
+++ js/script.js
@@ -6,4 +6,17 @@ function mostrarMenu() {
   document.getElementById("top-menu").id= "top-menu-mobile-menu";
 }
 
+function ocultarMenu() {
+  document.getElementById("page-header-mobile-menu").id = "page-header";
+  document.getElementById("logo-mobile-menu").id= "logo";
+  document.getElementById("hamburger-button-mobile-menu").id= "hamburger-button";
+  document.getElementById("close-btn-mobile-menu").id= "close-btn";
+  document.getElementById("top-menu-mobile-menu").id= "top-menu";
+}
+
+for(let x=0; x<document.getElementsByClassName("menu-item").length; x++)
+{
+  document.getElementsByClassName("menu-item")[x].addEventListener("click",ocultarMenu,false);
+}
+
 document.getElementById("hamburger-button").addEventListener("click",mostrarMenu);
